17795070fd61105ae0a0926ceb0755ba62c03dac
timtashpulatov <tim.tashpulatov@gmail.com>
2019-01-12 21:05:24 +0300

    Create ints.asm

---
+++ ints.asm
@@ -0,0 +1,58 @@
+ 	.project okeah
+	
+; Обработчик прерывания от Таймера 0	
+	.org 20h
+	
+	mvi     a, 20h
+	out     80h
+	ei
+	
+; Полезная работа	
+	lhld    COUNT
+	inx     h
+	shld    COUNT
+	
+	mov     a,l
+	cpi     2
+	jnz     NOTYET
+	lxi     h, 0
+	shld    COUNT
+	
+
+  lda     COLOR
+  inr     a
+  ani     3
+  sta     COLOR
+  ori     40h
+        
+  out     0e1h
+	
+NOTYET	
+	ret
+	
+COUNT   dw      0
+COLOR   db      41h
+	
+;	.org 8000h
+
+BANKING	equ	0c1h		; регистр управления банками ОЗУ и ПЗУ
+VIDEO	equ	0e1h		; регистр управления цветом и режимами видео
+
+
+        mvi     a, 36h
+        out     63h
+        mvi     a, 0aah
+        out     60h
+        mvi     a, 0eh
+        out     60h
+
+; OCW1
+        mvi     a, 0b11101111   ; разрешить прерывания от Таймера 0 (RST4)
+        out     81h
+
+        ei
+
+        jmp     .
+        
+	jmp	0e003h		; теплый старт "Монитора"
+
