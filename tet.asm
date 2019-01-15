618f78f24df0f4a809634512ef96bdf3394318ac
timtashpulatov <tim.tashpulatov@gmail.com>
2019-01-15 20:35:35 +0300

    Update tet.asm

--- tet.asm
+++ tet.asm
@@ -5,6 +5,50 @@ BANKING equ     0C1h
 MAP32K  equ     0x01
 ENROM   equ     0x10
 
+XY      equ     1010h
+SCREEN  equ     0c000h
 
+        org     1000h
 
+        lxi     h, BITMAP1
+        lxi     b, XY
+        call    PaintBitmap
         ret
+        
+; HL - адрес битмапа
+; BC - X и Y
+PaintBitmap
+        di
+        ; Отключаем ПЗУ для доступа к экранному ОЗУ
+        mvi     a, ENROM
+        out     BANKING
+        
+        push    h
+        lxi     h, SCREEN
+        mov     d, b
+        mvi     e, 0
+        dad     d       ; hl = SCREEN + X*256
+        mvi     d, 0
+        mov     e, c
+        dad     d       ; hl = hl + Y
+        pop     d       ; de = адрес битмапа
+
+        mvi     c, 8
+PBLoop  ldax    d
+        mov     m, a
+        inx     d
+        inx     h
+        dcr     c
+        jnz     PBLoop
+
+        ; Включаем ПЗУ обратно
+        xra     a
+        out     BANKING
+        
+        ei
+        ret
+        
+BITMAP1
+        db      0xaa, 0x55, 0xaa, 0x55, 0xaa, 0x55, 0xaa, 0x55
+        
+        
