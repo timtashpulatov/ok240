4c0c16e3bfeab034e7715b8804eb3c2840841013
timtashpulatov <tim.tashpulatov@gmail.com>
2019-02-17 14:19:04 +0300

    Update ints.asm

--- ints.asm
+++ ints.asm
@@ -29,16 +29,61 @@ BANKING	equ	0c1h		; регистр управления банками ОЗУ и
 VIDEO	equ	0e1h		; регистр управления цветом и режимами видео
 
 
+; Дождемся кадрового ретрейса
+WaitForVR
+        in      41h
+        ani     2
+        jz     WaitForVR
+; Переждем его
+WaitForVRDone
+        in      41h
+        ani     2
+        jnz     WaitForVRDone
+        
         mvi     a, 36h
         out     63h
-        mvi     a, 080h
+        mvi     a, 0h
         out     60h
-        mvi     a, 01h
+        mvi     a, 05h
         out     60h
 
 ; OCW1
         mvi     a, 0b11101111   ; разрешить прерывания от Таймера 0 (RST4)
         out     81h
+
+; Pattern
+        mvi     a, 10h
+        out     BANKING
+        
+        lxi     hl, 0c000h
+        mvi     c, 250
+        
+DoPat   
+        push    hl
+        mvi     m, 0
+        inr     h
+        mvi     m, 0
+        inr     h
+        mvi     m, 0
+        inr     h
+        mvi     m, 255
+        inr     h
+        mvi     m, 255
+        inr     h
+        mvi     m, 0
+        inr     h
+        mvi     m, 255
+        inr     h
+        mvi     m, 255
+        
+        pop     hl
+        inr     l
+        dcr     c
+        jnz     DoPat
+        
+        xra     a
+        out     BANKING
+        
 
         ei
 
