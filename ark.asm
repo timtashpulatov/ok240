e99da1c6acf41c76dcced16023c3f701749f8688
timtashpulatov <tim.tashpulatov@gmail.com>
2019-04-07 20:00:33 +0300

    Update ark.asm

--- ark.asm
+++ ark.asm
@@ -36,7 +36,7 @@ DEFAULTBattyDelay       equ     2
 BATTY_STOP      equ     0
 BATTY_RIGHT     equ     1
 BATTY_LEFT      equ     2
-DEFAULTBALLX    equ     28h     ;32
+DEFAULTBALLX    equ     4fh     ;32
 DEFAULTBALLY    equ     18h     ;224
 DEFAULTBALLDX   equ     0       ; debug Y first ; 1
 DEFAULTBALLDY   equ     1
