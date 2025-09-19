6d2d1f8df8707607273ccd6b83591b3651b29498
timtashpulatov <tim.tashpulatov@gmail.com>
2025-09-19 19:05:55 +0300

    Update hexload.asm

--- hexload.asm
+++ hexload.asm
@@ -1,6 +1,6 @@
 	.project read
 
-WARMBOOT        equ     0e003h
+HEXOUT          equ     0e003h
 COUT            equ     0e00ch
 RIN             equ     0e00fh
 
@@ -24,11 +24,12 @@ CR              equ     10
 LF              equ     13
 
 
-        .org    0dd1ch  ; Start address of READ user command in REL.8 ROM        
+        ; .org    0dd1ch  ; Start address of READ user command in REL.8 ROM        
+
+        .org 100h
 
         lxi     de, GREETING
-        mvi     c, C_WRITESTR
-        call    BDOS
+        call    PrintDE
 
 #ifdef pops
         
@@ -81,6 +82,11 @@ SetFileNameInFCB:
 
 #endif
         
+LENGTH  equ     0b208h        
+        
+        lxi     hl, 0
+        shld    LENGTH
+        
 WaitForLineBeginning
         call    RIN
         cpi     ':'
@@ -97,6 +103,22 @@ WaitForLineBeginning
         call    RIN_BYTE        ; address LO
         mov     l, a    
         call    RIN_BYTE        ; record type
+        
+
+
+        push    hl
+        lhld    LENGTH
+        mov     a, l
+        add     e
+        mov     l, a
+        mov     a, h
+        aci     0
+        mov     h,a
+        shld    LENGTH
+        pop     hl
+        
+        
+        
         
         mov     c, e
 ReceiveRecord
@@ -118,12 +140,22 @@ EndOfFile:
         jz      Done
 Error:        
         lxi     de, ERR
-        mvi     c, C_WRITESTR
-        call    BDOS
+        call    PrintDE
 Done:        
+        lhld    LENGTH
+        mov     a, h
+        call    HEXOUT
+        mov     a, l
+        call    HEXOUT
+
+        lxi     de, BytesRead
+        call    PrintDE
+
         mvi     c, P_TERMCPM
         jmp     BDOS
 
+BytesRead:
+        db      ' bytes.',CR,LF,'$'
 Err:    db      CR,LF,'?Err',CR,LF,'$'        
         
 RIN_BYTE:
@@ -161,14 +193,35 @@ CTN:
         adi     10
         ora     a
         ret
-        
+
+PrintDE:
+        mvi     c, C_WRITESTR
+        jmp    BDOS
+
+
 #ifdef pops        
+MyRIN:
+        in      0a1h
+        ani     2
+        jz      Machine
+        
+        in      0a0h
+        ani     7fh
+        ret
+        
+Machine:
+        in      0e1h
+        xri     38h
+        out     0e1h
+
+        jmp     MyRIN
+        
 FILEEXT_BEGIN:
         db      0,'12345678SAV'
 FILEEXT_END:        
 #endif
 
 GREETING:
-        db      CR,LF,'Reading HEX via RS232 interface...','$'
+        db      CR,LF,'Read HEX from RS232... ','$'
 
         end
