b4dc2212c722f8a1b140cebed8c28e534ed4e23a
timtashpulatov <tim.tashpulatov@gmail.com>
2025-09-28 14:20:38 +0300

    Update rom.asm

--- rom.asm
+++ rom.asm
@@ -1,3 +1,11 @@
+        .project rom.com
+        .org    100h
+
+#define WITH_DELAY
+#define USE_DB64 ; see resource.inc (Alt+2)
+#define BDOS(fn) mvi c, fn \ call bdos
+#define WRITESTR(str) lxi d, str \ BDOS(9)
+
 #define PORTA           00h
 #define PORTB           01h
 #define PORTC           02h
@@ -8,6 +16,31 @@
         ; db      'AB'
         ; mov     b, c
         ; mov     b, d
+        
+; Read block from page 0, address 0
+; 
+BLOCK_LEN       equ     512
+DEST_ADDR       equ     8000h
+ReadLoader:
+        call    InitUserPort
+        xra     a
+        call    SetPage
+        
+        lxi     hl, 0
+        lxi     de, DEST_ADDR
+        lxi     bc, BLOCK_LEN
+
+ReadBlockLoop:
+        call    ReadByte
+        stax    d
+        inx     de
+        inx     hl
+        dcx     bc
+        mov     a, b
+        ora     c
+        jnz     ReadBlockLoop
+        ret
+        
 InitUserPort:
         mvi     a, 0ffh  ; port A input (data). ports B and C output (address and paging)
         out     PORT_CTRL
@@ -23,37 +56,14 @@ SetPage:
         out     PORTC
         ret
 
-; Address in HL
-SetAddress:
+; Address in HL, return result in A
+ReadByte:
         mov     a, l
         out     PORTB           ; address bits A7..A0
         mov     a, h
         ani     7fh
         out     PORTC           ; address bits A14..A8
-        ret
-
-ReadByte:
         in      PORTA
         ret
 
-; Read block from page 0, address 0
-; 
-BLOCK_LEN       equ     512
-DEST_ADDR       equ     100h
-ReadLoader:
-        call    InitUserPort
-        xra     a
-        call    SetPage
-        lxi     de, DEST_ADDR
-        lxi     bc, BLOCK_LEN
-
-ReadBlockLoop:
-        call    SetAddress
-        call    ReadByte        ; TODO join with SetAddress
-        stax    d
-        inx     de
-        dcx     bc
-        mov     a, b
-        ora     c
-        jnz     ReadBlockLoop
-        ret
+        
