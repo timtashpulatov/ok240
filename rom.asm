4310db4f734b50cc31568cdcf95e84e8c21f4ea2
timtashpulatov <tim.tashpulatov@gmail.com>
2025-09-28 12:54:49 +0300

    Update rom.asm

--- rom.asm
+++ rom.asm
@@ -1,9 +1,9 @@
-
 #define PORTA           00h
 #define PORTB           01h
 #define PORTC           02h
 #define PORT_CTRL       03h
 
+#define PC7             0b10000000
 
         ; db      'AB'
         ; mov     b, c
@@ -12,22 +12,23 @@
         mvi     a, 0ffh  ; port A input (data). ports B and C output (address and paging)
         out     PORT_CTRL
 
-; Address in HL
-SetAddress:
-        mov     a, l
-        out     PORTB
-        mov     a, h
-        out     PORTC
-
 ; Set ROM page (page number in C)
 SetPage:        
         mov     a, c
-        ani     0b01111111
+        ani     0fh             ; pages 0..15
         out     PORTC
-        ori     0b10000000
+        ori     0b10000000      ; strobe high
         out     PORTC
-        ani     0b01111111
+        ani     0b01111111      ; strobe low
         out     PORTC
+
+; Address in HL
+SetAddress:
+        mov     a, l
+        out     PORTB           ; address bits A7..A0
+        mov     a, h
+        ani     7fh
+        out     PORTC           ; address bits A14..A8
 
 ReadByte:
         in      PORTA
