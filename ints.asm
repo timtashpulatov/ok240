f30786de3a400deb7cc5f61a0a6789aab8fa2804
timtashpulatov <tim.tashpulatov@gmail.com>
2025-10-30 23:22:58 +0200

    Update ints.asm

--- ints.asm
+++ ints.asm
@@ -3,7 +3,29 @@
 BANKING	equ	0c1h		; регистр управления банками ОЗУ и ПЗУ
 VIDEO	equ	0e1h		; регистр управления цветом и режимами видео
 
-	.org 100h
+; делитель на 768 тактовой частоты 1.5МГц канала 0 таймера даст смену палитры каждые 8 строк (ivagor)
+
+; DIV     equ     01e0h    ;       = 480  = 5 lines
+; 5 * 64us = 320us
+; 1500000 / 15625 = 96 = 1 line
+;
+
+DIV     equ     768     ; = 8 lines
+
+
+	.org    100h
+
+; Clear scroll
+        ; mvi     a, 7
+        ; out     0c0h
+        ; out     0c2h
+
+; Timer programming, part I
+        mvi     a, 36h
+        out     63h
+        mvi     a, DIV & 0xff
+        out     60h
+
 
 ; Дождемся кадрового ретрейса
         lxi     b, 0002
@@ -26,14 +48,7 @@ WaitForVR1
 ;         ani      1
 ;         jz      WaitForHR
 
-; делитель на 768 тактовой частоты 1.5МГц канала 0 таймера даст смену палитры каждые 8 строк (ivagor)
-
-DIV     equ     01e0h * 2   ;       = 480  = 5 lines
-
-        mvi     a, 36h
-        out     63h
-        mvi     a, DIV & 0xff
-        out     60h
+; Timer programming, part II
         mvi     a, DIV >> 8
         out     60h
 
@@ -111,19 +126,49 @@ DoPatSub
 TimerInterruptHandler	
         lda     COLOR
         out     VIDEO
+        ; inr     a
+        ; ; ani     3fh
+        ; ani     7
+        ; ; ori     40h
+        ; ori     48h
+        ; sta     COLOR
+        
+; счетчик
+        lda     COUNT
         inr     a
-        ; ani     3fh
-        ani     7
-        ori     40h
+        ani     15
+        sta     COUNT
+
+; используем счетчик для выборки очередной палитры
+        push    hl
+        push    bc
+        
+        lxi     hl, PALETTE_LIST
+        lxi     bc, 0
+        lda     COUNT
+        mov     c, a
+        dad     b
+        mov     a, m
         sta     COLOR
         
+        pop     bc
+        pop     hl
+
+; подтверждение прерывания        
 	mvi     a, 20h
 	out     80h
         ei
 	ret
-	
-COUNT   dw      0
-COLOR   db      41h
+
+PALETTE_LIST
+        db      40h, 40h, 42h, 43h, 44h, 45h, 46h, 47h
+        db      48h, 48h, 49h, 49h, 4ah, 4ah, 4bh, 4bh
+        db      40h, 41h, 42h, 43h, 44h, 45h, 46h, 47h
+        db      40h, 41h, 42h, 43h, 44h, 45h, 46h, 47h
+
+
+COUNT   db      0
+COLOR   db      40h
         
         
         
