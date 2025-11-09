927f39dfc4f2621ec3e38e143b402665ad5a71bb
timtashpulatov <tim.tashpulatov@gmail.com>
2025-11-09 14:14:58 +0200

    Update fdc.asm

--- fdc.asm
+++ fdc.asm
@@ -1,6 +1,10 @@
 	.project fdc
 	.org 100h
 
+CONST           equ     0e006h  ; A = FF if there is keypress
+CONIN           equ     0e009h  
+CONOUT          equ     0e00ch  ; output symbol from C
+
 PORT_CMD        equ     20h
 PORT_TRACK      equ     21h
 PORT_SECTOR     equ     22h
@@ -17,6 +21,14 @@ CMD_SEEK        equ     10h
         lxi     d, Help
         mvi     c, 9
         call     5
+
+
+        mvi     c, '?'
+        call    CONOUT
+
+        lxi     h, CurPosTest
+        call    PrintString
+
         
         call    WaitForKey
         
@@ -49,10 +61,43 @@ CMD_SEEK        equ     10h
 
         rst      0
 
+; ************************************************************************
+; Ожидаение готовности дисковода или остановки двигателя по таймауту
+; Возвращает A=00, если не готов или таймаут
+; ************************************************************************
+WaitForDriveReady      
+        in      PORT_FLOPPY
+        ani     80h                     ; check MOTST
+        rz
+
+        in      PORT_CMD
+        ani     80h                     ; 0x80 if drive NOT READY
+        jnz     WaitForDriveReady
+        
+        inr     a                       ; A = 1
+
+        ret
+
+
+
 WaitForKey
         mvi     c, 1
         jmp     5
-        
+
+
+PrintString
+        mov     a, m
+        ora     a
+        rz
+        mov     c, a
+        call    CONOUT
+        inx     h
+        jmp PrintString
+
+CurPosTest
+        db      1fh
+        db      27, '0', 100, 100, 31h
+        db      'ABC', 0
         
 Help:   db      'FDC v0.1', 10, 13
         db      'Usage:', '$'
